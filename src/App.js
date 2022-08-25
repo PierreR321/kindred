9c58466b2c5cc43c78691e2868bef725a09f2c21
Pierre L <109498793+PierreR321@users.noreply.github.com>
2022-08-25 04:44:37 -0700

    commit message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DropDown from "./components/DropDown";
+
+const App = () => {
+  return (
+    <div className="App">
+      KINDRED SPIRITS
+      <DropDown  isShown={false}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DropDown.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const DropDown = (props) => {
+    return (
+        <Container isShown={props.isShown}>
+            <DDTerm>MINT</DDTerm>
+            <DDTerm>TASTEMAKERS</DDTerm>
+            <DDTerm>VISION MAP</DDTerm>
+            <DDTerm>FAQ</DDTerm>
+            <DDTerm>TEAM</DDTerm>
+            <DDTerm>DISCORD</DDTerm>
+            <DDTerm>TWITTER</DDTerm>
+            <DDTerm>MEDIUM</DDTerm>
+            <DDTerm>CONNECT WALLET</DDTerm>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    position: fixed;
+    width: 100%;
+    display: ${(props) => props.isShown ? "inherit" : "none"}
+    padding: 100px 18px 0;
+    background: rgb(255, 243, 237);
+    opacity: 0.7;
+`
+
+const DDTerm = styled.div`
+    color: #956767;
+    font-size: 22.5px;
+    padding: 10.5px 0;
+    border-bottom: 1px solid black;
+`
+
+export default DropDown;
